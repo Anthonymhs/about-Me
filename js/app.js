6b7b759fb331487d655dcae7f09e4ea3047ab335
ANTHONY <anthonymhs0320@gmail.com>
2024-04-23 22:42:16 -0500

    arreglando el javaS

--- js/app.js
+++ js/app.js
@@ -1,84 +1,55 @@
 
 'use strict';
 
-let nombre = prompt('Por favor, ingresa tu nombre:');
+let nombre = prompt ('Por favor, ingresa tu nombre:');
 
 alert('Hola, un gusto. ' + nombre);
 
 let score=0;
 
-function preguntaUno (){
-    let meGustaBaskell = prompt('¿Me gusta el basketball? (si/no)');
-    console.log();
-    if (meGustaBaskell.toLowerCase() === 'si') {
-        alert('Es verdad, si me gusta.');
-        // console.log("Sabe que me gusta el baskell");
+function hacerPregunta(pregunta) {
+    let respuesta = prompt(pregunta + ' (si/no)').toLowerCase();
+    if (respuesta === 'si') {
+        alert('Es verdad, sí me gusta.');
         score++;
-    } else if (meGustaBaskell.toLowerCase() === 'no') {
-        alert('Que pena, en verdad si me gusta');
-        console.log('No sabe que me gusta el baskell');
-    }
-}
-function preguntaDos(){
-    let meGustaLeer = prompt('¿Me gusta leer? (si/no)');
-    if (meGustaLeer.toLowerCase() === 'si') {
-        alert('Es verdad, si me gusta.');
-        score++;
-        console.log('Sabe que me gusta leer');
-    } else if (meGustaLeer.toLowerCase() === 'no') {
-        alert('Que pena, en verdad si me gusta leer');
-        console.log('no sabe');
-    } 
-}
-function preguntaTres(){
-    let meGustaElCine = prompt('¿Me gusta el cine? (si/no)');
-    if (meGustaElCine.toLowerCase() === 'si') {
-        alert('Es verdad, si me gusta.');
-        console.log('Sabe que me gusta el cine');
-        score++;
-    } else if (meGustaElCine.toLowerCase() === 'no'){
-        alert('Que pena, en verdad si me gusta ver peliculas');
-        console.log('No sabe que me gusta el cine');
-    }
-}
-function preguntaCuatro(){
-    let preguntaFinal = prompt('Si ya sabemos que me gusta y que no me gusta, dependiendo de eso, ¿Crees que soy alguien de ver futbol? (si/no)');
-    if (preguntaFinal.toLowerCase() === 'si') {
-        alert('Pues no amigo/a, no veo partidos de futbol');
-        console.log('Me conoce bien');
-    } else if (preguntaFinal.toLowerCase() === 'no'){
-        alert('Perfecto, ya nos conocemos mejor');
-        console.log('No me conoce muy bien');
-        score++;
+    } else if (respuesta === 'no') {
+        alert('Que pena, en verdad sí me gusta ' + pregunta.toLowerCase());
     }
 }
 
-preguntaUno(), preguntaDos(), preguntaTres(), preguntaCuatro ();
 
-let añoDeNacimiento ;
+let preguntas = [
+    '¿Me gusta el basketball?',
+    '¿Me gusta leer?',
+    '¿Me gusta el cine?',
+    '¿Me gustan los video juegos?'
+    'Si ya sabemos que me gusta y que no me gusta, dependiendo de eso ¿Crees que soy alguien de ver futbol?'];
+
+for (let i = 0; i < preguntas.length; i++) {
+    hacerPregunta(preguntas[i]);
+}
+
+
+let añoDeNacimiento;
 let attempts = 0;
 
-while (añoDeNacimiento != 2001 && attempts < 4) {
-    añoDeNacimiento = prompt('¿En qué año nací entre (1999/2005)');
+while (añoDeNacimiento !== 2001 && attempts < 4) {
+    añoDeNacimiento = parseInt(prompt('¿En qué año nací entre (1999/2005)'));
     if (añoDeNacimiento < 2001){
-        alert ('No tan viejo amigo/a');
-        console.log ('No tan viejo amigo/a');
+        alert('No tan viejo amigo/a');
     } else if (añoDeNacimiento > 2001) {
-        alert ('No soy tan joven');
-        console.log('No soy tan joven');
+        alert('No soy tan joven');
     } else {
-        alert ('Es correcto')
-        console.log('Es correcto')
+        alert('Es correcto');
         score++;
         break;
-    }´
+    }
     attempts++;
-    if(attempts=4){
-        alert('te quedastes si nintetno la res')
+    if(attempts === 4) {
+        alert('Te quedaste sin intentos');
     }
 }
-console.log ('Nací el 2001')
-alert ('Nací el 2001')
+alert('Nací en 2001');
 
 function preguntaSiete() {
     let animalesFavoritos = ['perro','gato','hamster'];
@@ -90,16 +61,15 @@ function preguntaSiete() {
         for (let i = 0; i < animalesFavoritos.length; i++) {
             if (animal == animalesFavoritos[i]) {
                 alert ('adivinaste!');
-                console.log ('adivinaste!');
                 attempts = 6;
                 break;
             } else{
-                console.log (animal, '¡Incorrecto!, intento' + attempts + ' de 6');
+                alert (animal, '¡Incorrecto!, intento' + attempts + ' de 6');
             }
         }
         attempts++;
     }
-console.log ('mis animales favoritos son ' + animalesFavoritos);
+alert ('mis animales favoritos son ' + animalesFavoritos);
 }
 preguntaSiete();
 
